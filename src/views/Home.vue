1544d410e12cdfd09bc3a440a38a2e7b57bfe804
alek101 <47416711+alek101@users.noreply.github.com>
2021-02-18 16:49:58 +0100

    ver 1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet nihil non aspernatur perferendis qui accusantium sit ut, odit, laboriosam, consectetur vel. Et dignissimos voluptatem eveniet vero, distinctio accusantium obcaecati impedit?
+    Maxime accusantium sit magni libero nostrum, minus dolore ratione deleniti reiciendis, rerum mollitia, dolor maiores. Odio, dolor cumque quo fugit aliquam tempora magnam velit. Provident nulla id nemo dolore doloribus?
+    Ipsum ab necessitatibus temporibus sed iste deserunt aut est possimus magnam perspiciatis ut amet ratione laborum id quis earum, quos alias sit corrupti adipisci modi error? Eligendi id rerum laborum?
+    Explicabo corporis in aliquam eaque! Quos dicta deleniti dolor delectus neque pariatur laudantium deserunt, laboriosam nemo repellendus! Cumque tempore repudiandae exercitationem repellendus voluptatibus, autem blanditiis beatae rerum dolorum in nesciunt.
+    Quibusdam tenetur deleniti numquam a voluptatem, voluptate suscipit omnis consectetur vero totam repudiandae exercitationem possimus magnam tempore, ullam quas ducimus? Aliquam, ab cupiditate! Maiores sunt quidem iste quis totam. Vel.
+    Ex libero nobis corporis possimus, cumque obcaecati aliquam culpa commodi expedita vitae odit, dolores minus asperiores. Sit distinctio voluptatum, quibusdam inventore assumenda nihil, provident facilis cum laudantium commodi, eveniet ipsum?
+    Suscipit ipsum dolore temporibus provident magnam animi earum tempora quis, dolorem repellat aut, doloremque eos, deserunt quidem perspiciatis mollitia ad? Ad qui consequuntur quas aut magnam quod iusto corporis laudantium.
+    Ex quidem molestias ut dignissimos praesentium voluptas pariatur corporis mollitia labore repellendus, dolorem nam qui itaque architecto incidunt dolor quisquam nostrum, aliquam eos suscipit. Qui molestiae aspernatur ratione officiis ex!
+    Labore maxime quos maiores, rem fugit voluptates ducimus totam nisi corrupti vel, impedit porro eveniet facere tempora natus et? Eos iure deserunt quia voluptate provident nulla soluta quaerat ipsam doloribus?
+    Adipisci velit aut asperiores nemo quo corrupti modi eaque omnis ratione, repudiandae consequatur aperiam nisi enim, nesciunt tempore, quod at! Aliquid cumque odio nisi accusantium dignissimos libero vitae autem veniam.</p>
+
+     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet nihil non aspernatur perferendis qui accusantium sit ut, odit, laboriosam, consectetur vel. Et dignissimos voluptatem eveniet vero, distinctio accusantium obcaecati impedit?
+    Maxime accusantium sit magni libero nostrum, minus dolore ratione deleniti reiciendis, rerum mollitia, dolor maiores. Odio, dolor cumque quo fugit aliquam tempora magnam velit. Provident nulla id nemo dolore doloribus?
+    Ipsum ab necessitatibus temporibus sed iste deserunt aut est possimus magnam perspiciatis ut amet ratione laborum id quis earum, quos alias sit corrupti adipisci modi error? Eligendi id rerum laborum?
+    Explicabo corporis in aliquam eaque! Quos dicta deleniti dolor delectus neque pariatur laudantium deserunt, laboriosam nemo repellendus! Cumque tempore repudiandae exercitationem repellendus voluptatibus, autem blanditiis beatae rerum dolorum in nesciunt.
+    Quibusdam tenetur deleniti numquam a voluptatem, voluptate suscipit omnis consectetur vero totam repudiandae exercitationem possimus magnam tempore, ullam quas ducimus? Aliquam, ab cupiditate! Maiores sunt quidem iste quis totam. Vel.
+    Ex libero nobis corporis possimus, cumque obcaecati aliquam culpa commodi expedita vitae odit, dolores minus asperiores. Sit distinctio voluptatum, quibusdam inventore assumenda nihil, provident facilis cum laudantium commodi, eveniet ipsum?
+    Suscipit ipsum dolore temporibus provident magnam animi earum tempora quis, dolorem repellat aut, doloremque eos, deserunt quidem perspiciatis mollitia ad? Ad qui consequuntur quas aut magnam quod iusto corporis laudantium.
+    Ex quidem molestias ut dignissimos praesentium voluptas pariatur corporis mollitia labore repellendus, dolorem nam qui itaque architecto incidunt dolor quisquam nostrum, aliquam eos suscipit. Qui molestiae aspernatur ratione officiis ex!
+    Labore maxime quos maiores, rem fugit voluptates ducimus totam nisi corrupti vel, impedit porro eveniet facere tempora natus et? Eos iure deserunt quia voluptate provident nulla soluta quaerat ipsam doloribus?
+    Adipisci velit aut asperiores nemo quo corrupti modi eaque omnis ratione, repudiandae consequatur aperiam nisi enim, nesciunt tempore, quod at! Aliquid cumque odio nisi accusantium dignissimos libero vitae autem veniam.</p>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Job Details Page</h1>
+  <p>The Job id is {{ $route.params.id}}</p>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    //becuase we accepted props in router, as props:true, now we can use props insted of data
+    // data() {
+    //     return {
+    //         id: this.$route.params.id
+    //     }
+    // }
+}
+</script>
+
+<style>
+
+</style>
